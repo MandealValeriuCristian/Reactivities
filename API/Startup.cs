f82d97332960c8468f444c1116fce028546e996c
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-11-01 01:30:48 +0200

    38. Adding AutoMapper

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Activities;
+using Application.Core;
+using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -50,6 +52,7 @@ namespace API
                 });
             });
             services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -63,6 +66,7 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
+            app.UseDeveloperExceptionPage();
 
             app.UseRouting();
 
--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Persistence;
@@ -15,17 +16,20 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
             {
+                _mapper = mapper;
                 _context = context;
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
-              var activity = await _context.Activities.FindAsync(request.Activity.Id);
-              activity.Title = request.Activity.Title ?? activity.Title;
-              await _context.SaveChangesAsync();
-              return Unit.Value;
+                var activity = await _context.Activities.FindAsync(request.Activity.Id);
+                //activity.Title = request.Activity.Title ?? activity.Title; - manual mapping
+                _mapper.Map(request.Activity, activity); // auto mapping
+                await _context.SaveChangesAsync();
+                return Unit.Value; 
             }
         }
     }
