b533fd838d84823020f9efee5f71fdf25d38546c
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-10-31 15:28:10 +0200

    CORS Policy

---
+++ client-app/build/precache-manifest.63355738ab9df22d01a858f53a39892c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e98b32b55a1417e0c124fe48c24fe8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6deabbea7dd0ddfb6901",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "8e55f3a0b6de2a52972d",
+    "url": "/static/js/2.1abed59d.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.1abed59d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8a7e58d2c2205364293",
+    "url": "/static/js/3.1823ed6d.chunk.js"
+  },
+  {
+    "revision": "6deabbea7dd0ddfb6901",
+    "url": "/static/js/main.3e593247.chunk.js"
+  },
+  {
+    "revision": "e03c466651a1a7fcc447",
+    "url": "/static/js/runtime-main.f1a9ee12.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
