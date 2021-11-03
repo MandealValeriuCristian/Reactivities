cfa0c344600ce4eff5db44c292ac5d496f2a50a7
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-11-03 07:33:14 +0200

    53. Selecting an activity to view

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -6,17 +6,28 @@ import NavBar from './navbar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 function App() {
   const [activities, setActivities] =useState<Activity[]>([]);
-
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);  
   useEffect(()=>{
     axios.get<Activity[]>('http://localhost:5000/api/activities').then(response =>{
       setActivities(response.data)
     })
   }, [])
+  function handleSelectActivity(id: string)
+  {
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+  function handleCancelActivity(){
+    setSelectedActivity(undefined);
+  }
   return (
     <Fragment> /* short version of fragment <> ... </> */
         <NavBar/>
        <Container style={{marginTop: '7em'}}>
-          <ActivityDashboard activities={activities}/>
+          <ActivityDashboard 
+          activities={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelActivity}/>
        </Container>
     </Fragment>
   );
