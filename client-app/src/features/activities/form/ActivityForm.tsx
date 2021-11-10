b6cbf8f57231e7d9f33fd30f14dc81cbea68565a
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-11-10 22:36:31 +0200

    58. Using a GUID for the activity id

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, ChangeEventHandler, useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { Button, Form, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 interface Props{
