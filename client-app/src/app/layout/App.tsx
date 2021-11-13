ed5551f8929fe323ee9ca97b4a908b7e31fcb23b
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-11-13 09:40:31 +0200

    63. Axios types

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -11,7 +11,13 @@ function App() {
   const [editMode, setEditMode] =useState(false);
   useEffect(()=>{
     agent.Activities.list().then(response =>{
-      setActivities(response)
+      let activities: Activity[] = [];
+      response.forEach( activity =>{
+        activity.date = activity.date.split('T')[0];
+        activities.push(activity);      }
+
+      )
+      setActivities(activities)
     })
   }, [])
   function handleSelectActivity(id: string)
