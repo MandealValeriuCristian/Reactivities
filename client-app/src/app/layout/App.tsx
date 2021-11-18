6fadec19cd91b1cd5e49eb5c68d34290ce5eeeee
Valeriu-Cristian Mandeal <mandealvcristian@gmail.com>
2021-11-18 22:26:06 +0200

    76. Deleting an activity using MobX

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,44 +1,23 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import {Container} from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './navbar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
 function App() {
   const{activityStore} = useStore();
-
-  const [activities, setActivities] =useState<Activity[]>([]);
-  const [submitting, setSubmitting] = useState(false);
   useEffect(()=>{
     activityStore.loadActivities();
   }, [activityStore])
-  
-  
-  function handleDeleteActivity(id: string)
-  {
-    setSubmitting(true);
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter(x => x.id !== id)])
-      setSubmitting(false);
-      })
-    
-  }
-
+ 
   if(activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
 
   return (
     <Fragment> /* short version of fragment <> ... </> */
         <NavBar/>
        <Container style={{marginTop: '7em'}}>
-        
-          <ActivityDashboard 
-          activities={activityStore.activities}
-          deleteActivity={handleDeleteActivity}
-          submitting={submitting}
-          />
+          <ActivityDashboard />
        </Container>
     </Fragment>
   );
