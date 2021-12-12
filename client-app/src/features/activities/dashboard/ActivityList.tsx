aec10162f2ff5ae60fb540a7fde685c0af9f3621
Mandeal Valeriu-Cristian <mandealvcristian@gmail.com>
2021-12-12 22:29:19 +0200

    99. Styling the home page

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,6 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import React, { Fragment } from 'react';
-import { Header, Item, Segment } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import ActivityListItem from './ActivityListItem';
 
@@ -9,25 +9,18 @@ import ActivityListItem from './ActivityListItem';
 export default observer(function ActivityList() {
     const { activityStore } = useStore();
     const { groupedActivities } = activityStore;
-
-
-
     return (
         <>
             {groupedActivities.map(([group, activities]) => (
                 <Fragment key={group}>
                     <Header sub color='teal'>
                         {group}
-                    </Header>
-                
+                    </Header>                
                             {activities.map(activity => (
                                 <ActivityListItem key={activity.id} activity={activity} />
-                            ))}
-                        
+                            ))}                        
                 </Fragment>
             ))}
         </>
-
-
     )
 })
