dbf13d1ed4da99aa9ea18ccf8c4d3d4507ec2e64
Mandeal Valeriu-Cristian <mandealvcristian@gmail.com>
2021-12-14 10:16:45 +0200

    111. Adding a not found component

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,12 +3,13 @@ import {Container} from 'semantic-ui-react';
 import NavBar from './navbar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
-import { Route, useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import HomePage from '../../features/activities/home/HomePage';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 import TestErrors from '../../features/errors/TestError';
 import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
 function App() {
   const location = useLocation();
   return (
@@ -22,12 +23,14 @@ function App() {
         <>
         <NavBar/>
        <Container style={{marginTop: '7em'}}>
-         
+         <Switch>
          <Route exact path='/activities' component={ActivityDashboard}/>
          <Route path='/activities/:id' component={ActivityDetails}/>
          <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm}/>
          <Route path='/errors' component={TestErrors} />
-       </Container>
+         <Route component={NotFound}/>       
+         </Switch>
+        </Container>
         </>
       )}
       />
