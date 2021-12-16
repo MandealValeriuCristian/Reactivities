b6889aa47e9e9fc6dbab7dda398e79e805363a2b
Mandeal Valeriu-Cristian <mandealvcristian@gmail.com>
2021-12-16 23:02:27 +0200

    126. Hooking up the form submission to Formik

--- client-app/src/features/errors/TestError.tsx
+++ client-app/src/features/errors/TestError.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import {Button, Header, Segment} from "semantic-ui-react";
+import { Button, Header, Segment } from "semantic-ui-react";
 import axios from 'axios';
-import { useStaticRendering } from 'mobx-react-lite';
 import ValidationErrors from './ValidationErrors';
 
 export default function TestErrors() {
@@ -46,7 +45,7 @@ export default function TestErrors() {
                 </Button.Group>
             </Segment>
             {errors &&
-            <ValidationErrors errors={errors} />
+                <ValidationErrors errors={errors} />
             }
         </>
     )
