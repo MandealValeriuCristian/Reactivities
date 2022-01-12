393570cea38b1d98c491e445eac4d60a8bd22178
Mandeal Valeriu Cristian <mandealvaleriucristian@gmail.com>
2022-01-12 12:59:54 +0200

    149. Setting the token upon login

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -1,22 +1,32 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
+import { history } from "../../index";
 import agent from "../api/agent";
 import { User, UserFormValues } from "../models/user";
+import { store } from "./store";
 
 export default class UserStore {
-    user: User | null = null;
-    constructor() {
-        makeAutoObservable(this)
-    }
+  user: User | null = null;
+  constructor() {
+    makeAutoObservable(this);
+  }
 
-    get isLoggedIn(){
-        return !!this.user;
+  get isLoggedIn() {
+    return !!this.user;
+  }
+  login = async (creds: UserFormValues) => {
+    try {
+      const user = await agent.Account.login(creds);
+      store.commonStore.setToken(user.token);
+      runInAction(() => (this.user = user));
+      history.push("/activities");
+    } catch (error) {
+      throw error;
     }
-    login = async(creds: UserFormValues) => {
-        try {
-            const user = await agent.Account.login(creds);
-            console.log(user);
-        } catch (error) {
-            throw error;
-        }
-    }
+  };
+  logout = () => {
+    store.commonStore.setToken(null);
+    window.localStorage.removeItem("jwt");
+    this.user = null;
+    history.push("/");
+  };
 }
