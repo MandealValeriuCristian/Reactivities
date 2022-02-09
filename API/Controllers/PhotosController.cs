310d9a1cf0b91b4a42b097b3423fc56955f12838
Mandeal Valeriu Cristian <mandealvaleriucristian@gmail.com>
2022-02-09 22:13:41 +0200

    186. Deleting photos

--- API/Controllers/PhotosController.cs
+++ API/Controllers/PhotosController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Application.Photos;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +11,11 @@ namespace API.Controllers
         public async Task<IActionResult> Add([FromForm] Add.Command command)
         {
             return HandleResult(await Mediator.Send(command));
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+           return HandleResult(await Mediator.Send(new Delete.Command{Id = id}));
         }
     }
 }
