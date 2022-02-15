fa7e8d97223adbd830a1bba13a070ad61f2679da
Mandeal Valeriu Cristian <mandealvaleriucristian@gmail.com>
2022-02-15 18:13:13 +0200

    218. Sending comments

--- client-app/src/features/activities/details/ActivityDetailedChat.tsx
+++ client-app/src/features/activities/details/ActivityDetailedChat.tsx
@@ -1,18 +1,20 @@
+import { Formik, Form } from 'formik'
 import { observer } from 'mobx-react-lite'
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import {Segment, Header, Comment, Form, Button} from 'semantic-ui-react'
+import { Segment, Header, Comment, Button } from 'semantic-ui-react'
 import { useStore } from '../../../app/stores/store'
+import MyTextArea from '../form/MyTextArea'
 
 interface Props {
     activityId: string;
 }
 
-export default observer(function ActivityDetailedChat({activityId}: Props) {
+export default observer(function ActivityDetailedChat({ activityId }: Props) {
     const { commentStore } = useStore();
 
     useEffect(() => {
-        if(activityId){
+        if (activityId) {
             commentStore.createHubConnection(activityId);
         }
         return () => {
@@ -27,34 +29,48 @@ export default observer(function ActivityDetailedChat({activityId}: Props) {
                 attached='top'
                 inverted
                 color='teal'
-                style={{border: 'none'}}
+                style={{ border: 'none' }}
             >
                 <Header>Chat about this event</Header>
             </Segment>
-            <Segment attached>
+            <Segment attached clearing>
                 <Comment.Group>
-                    {commentStore.comments.map(comment => {
+                    {commentStore.comments.map(comment => (
                         <Comment key={comment.id}>
-                        <Comment.Avatar src={comment.image || '/assets/user.png'}/>
-                        <Comment.Content>
-                            <Comment.Author as={Link} to={`/profiles/${comment.username}`}>
-                                {comment.displayName}</Comment.Author>
-                            <Comment.Metadata>
-                                <div>{comment.createdAt}</div>
-                            </Comment.Metadata>
-                            <Comment.Text>{comment.body}</Comment.Text>
-                        </Comment.Content>
-                    </Comment>
-                    })}
-                    <Form reply>
-                        <Form.TextArea/>
-                        <Button
-                            content='Add Reply'
-                            labelPosition='left'
-                            icon='edit'
-                            primary
-                        />
-                    </Form>
+                            <Comment.Avatar src={comment.image || '/assets/user.png'} />
+                            <Comment.Content>
+                                <Comment.Author as={Link} to={`/profiles/${comment.username}`}>
+                                    {comment.displayName}</Comment.Author>
+                                <Comment.Metadata>
+                                    <div>{comment.createdAt}</div>
+                                </Comment.Metadata>
+                                <Comment.Text>{comment.body}</Comment.Text>
+                            </Comment.Content>
+                        </Comment>
+                    ))}
+
+                    <Formik
+                        onSubmit={(values, { resetForm }) =>
+                            commentStore.addComment(values).then(() => resetForm())}
+                        initialValues={{ body: '' }}
+                    >
+                        {({ isSubmitting, isValid }) => (
+                            <Form className='ui form'>
+                                <MyTextArea placeholder='Add comment' name='body' rows={2} />
+                                <Button
+                                    loading={isSubmitting}
+                                    disabled={isSubmitting || !isValid}
+                                    content='Add Reply'
+                                    labelPosition='left'
+                                    icon='edit'
+                                    primary
+                                    type='submit'
+                                    floated='right'
+                                />
+                            </Form>
+                        )}
+                    </Formik>
+
                 </Comment.Group>
             </Segment>
         </>
