e8f481521d30d9dcd4141dac0c5a8d3cb6abb266
Valeriu-Cristian Mandeal <mandealvaleriucristian@gmail.com>
2023-02-24 15:26:27 +0200

    Production ConnectionString

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -40,20 +40,20 @@ namespace API.Extensions
                 else
                 {
                     // Use connection string provided at runtime.
-                    connStr = Environment.GetEnvironmentVariable("DATABASE_URL");
+                    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
                     // Parse connection URL to connection string for Npgsql
-                    // connUrl = connUrl.Replace("postgres://", string.Empty);
-                    // var pgUserPass = connUrl.Split("@")[0];
-                    // var pgHostPortDb = connUrl.Split("@")[1];
-                    // var pgHostPort = pgHostPortDb.Split("/")[0];
-                    // var pgDb = pgHostPortDb.Split("/")[1];
-                    // var pgUser = pgUserPass.Split(":")[0];
-                    // var pgPass = pgUserPass.Split(":")[1];
-                    // var pgHost = pgHostPort.Split(":")[0];
-                    // var pgPort = pgHostPort.Split(":")[1];
+                    connUrl = connUrl.Replace("postgres://", string.Empty);
+                    var pgUserPass = connUrl.Split("@")[0];
+                    var pgHostPortDb = connUrl.Split("@")[1];
+                    var pgHostPort = pgHostPortDb.Split("/")[0];
+                    var pgDb = pgHostPortDb.Split("/")[1];
+                    var pgUser = pgUserPass.Split(":")[0];
+                    var pgPass = pgUserPass.Split(":")[2];
+                    var pgHost = pgHostPort.Split(":")[0];
+                    var pgPort = pgHostPort.Split(":")[1];
 
-                    // connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
                 }
                 // Whether the connection string came from the local development configuration file
                 // or from the environment variable, use it to set up your DbContext.
