fe93593f146f319572be23acd9e0a0fd4a50fd80
Valeriu-Cristian Mandeal <mandealvaleriucristian@gmail.com>
2023-02-24 16:04:39 +0200

    Allow any origin

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -67,7 +67,8 @@ namespace API.Extensions
                             .AllowAnyMethod()
                             .AllowAnyHeader()
                             .AllowCredentials()
-                            .WithOrigins("http://localhost:3000");
+                            //.WithOrigins("http://localhost:3000");
+                            .AllowAnyOrigin();
                     });
                 });
                 
