5b5f1919e3bb75c115db1a70ccc0057a828fd7dd
Valeriu-Cristian Mandeal <mandealvaleriucristian@gmail.com>
2023-02-27 13:28:56 +0200

    ConnectionString changed

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -45,13 +45,13 @@ namespace API.Extensions
                     // Parse connection URL to connection string for Npgsql
                     connUrl = connUrl.Replace("postgres://", string.Empty);
                     var pgUserPass = connUrl.Split("@")[0];
-                    var pgHostPortDb = connUrl.Split("@")[1];
+                    var pgHostPortDb = connUrl.Split("@[")[1];
                     var pgHostPort = pgHostPortDb.Split("/")[0];
-                    var pgDb = pgHostPortDb.Split("/")[1];
+                    var pgDb = pgHostPortDb.Split("]:")[1];
                     var pgUser = pgUserPass.Split(":")[0];
                     var pgPass = pgUserPass.Split(":")[1];
-                    var pgHost = pgHostPort.Split(":")[0];
-                    var pgPort = pgHostPort.Split(":")[1];
+                    var pgHost = pgHostPort.Split("]")[0];
+                    var pgPort = pgHostPort.Split("]:")[1];
 
                     connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL mode=Require;";
                 }
