aa101e77ab472d1c8c10673a45b0859d5122ac63
Valeriu-Cristian Mandeal <mandealvaleriucristian@gmail.com>
2023-02-28 11:53:59 +0200

    Getting ready for vercel

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -43,17 +43,17 @@ namespace API.Extensions
                     var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
                     // Parse connection URL to connection string for Npgsql
-                    connUrl = connUrl.Replace("postgres://", string.Empty);
+                    connUrl = connUrl.Replace("postgresql://", string.Empty);
                     var pgUserPass = connUrl.Split("@")[0];
-                    var pgHostPortDb = connUrl.Split("@[")[1];
+                    var pgHostPortDb = connUrl.Split("@")[1];
                     var pgHostPort = pgHostPortDb.Split("/")[0];
-                    var pgDb = pgHostPortDb.Split("]:")[1];
+                    var pgDb = pgHostPortDb.Split("/")[1];
                     var pgUser = pgUserPass.Split(":")[0];
                     var pgPass = pgUserPass.Split(":")[1];
-                    var pgHost = pgHostPort.Split("]")[0];
-                    var pgPort = pgHostPort.Split("]:")[1];
+                    var pgHost = pgHostPort.Split(":")[0];
+                    var pgPort = pgHostPort.Split(":")[1];
 
-                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};Trust Server Certificate=true;";
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
                 }
 
                 // Whether the connection string came from the local development configuration file
